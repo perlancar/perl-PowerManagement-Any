b22706bd0a9746c0a65bbf45401a50bbf46e3af3
perlancar (on pc-office) <perlancar@gmail.com>
2019-05-22 15:03:00 +0700

    Release v0.001

--- lib/PowerManagement/Any.pm
+++ lib/PowerManagement/Any.pm
@@ -18,58 +18,82 @@ $SPEC{':package'} = {
     summary => 'Common interface to some power management tasks',
 };
 
+# XXX probably should refactor this into Systemd::Util later
+sub _target_is_masked {
+    my $target = shift;
+
+    my ($out, $err);
+    system({capture_stdout=>\$out, capture_stderr=>\$err},
+           "systemctl", "status", $target);
+    # systemctl status always returns exit code=3, even for unknown target?
+    #if ($?) {
+    #    return [500, "Error when running 'systemctl status sleep.target'".
+    #                ": \$?=$?, stderr=$err"];
+    #}
+    $out =~ /^\s*Loaded: ([^(]+)/m or do {
+        return [412, "Cannot parse 'systemctl status $target': $out"];
+    };
+    my $loaded_status = $1;
+    my $is_masked;
+    if ($loaded_status =~ /not-found/) {
+        return [412, "System does not have $target"];
+    } elsif ($loaded_status =~ /masked/) {
+        $is_masked = 1;
+    } elsif ($loaded_status =~ /loaded/) {
+        $is_masked = 0;
+    } else {
+        return [412, "Unrecognized loaded status of $target: ".
+                    "$loaded_status"];
+    }
+    [200, "OK", $is_masked];
+}
+
 sub _prevent_or_unprevent_sleep {
     my ($which, %args) = @_;
 
   SYSTEMD:
     {
         log_trace "Checking if system is running systemd ...";
+
+        my ($res, $is_masked);
+
         require Systemd::Util;
-        my $res = Systemd::Util::systemd_is_running();
+        $res = Systemd::Util::systemd_is_running();
         unless ($res->[0] == 200 && $res->[2]) {
             log_trace "systemd is not running or cannot determine, ".
-                "skipped using systemd";
+                "skipped using systemd (%s)", $res;
             last;
         }
 
-        # XXX probably should refactor this into Systemd::Util later
-        my ($out, $err);
-        system({capture_stdout=>\$out, capture_stderr=>\$err},
-               "systemctl", "status", "sleep.target");
-        if ($?) {
-            return [500, "Error when running 'systemctl status sleep.target'".
-                        ": \$?=$?, stderr=$err"];
+        $res = _target_is_masked("sleep.target");
+        unless ($res->[0] == 200) {
+            return $res;
         }
-        $out =~ /^\s*Loaded: ([^(]+)/m or do {
-            return [412, "Cannot parse 'systemctl status sleep.target'"];
-        };
-        my $loaded_status = $1;
-        my $is_masked;
-        if ($loaded_status =~ /not-found/) {
-            return [412, "System does not have 'sleep.target'"];
-        } elsif ($loaded_status =~ /masked/) {
-            $is_masked = 1;
-        } elsif ($loaded_status =~ /loaded/) {
-            $is_masked = 0;
-        } else {
-            return [412, "Unrecognized loaded status of 'sleep.target': ".
-                        "$loaded_status"];
-        }
-
+        $is_masked = $res->[2];
         return [304, "sleep.target already masked"]
             if $which eq 'prevent' && $is_masked;
         return [304, "sleep.target already unmasked"]
             if $which eq 'unprevent' && !$is_masked;
 
         my $action = $which eq 'prevent' ? 'mask' : 'unmask';
+        my ($out, $err);
         system({capture_stdout=>\$out, capture_stderr=>\$err},
                "systemctl", $action, "sleep.target");
-        if ($?) {
-            return [500, "Error when running 'systemctl $action sleep.target'".
-                        ": \$?=$?, stderr=$err"];
-        }
+        #if ($?) {
+        #    return [500, "Error when running 'systemctl $action sleep.target'".
+        #                ": \$?=$?, stderr=$err"];
+        #}
 
-        # XXX check if target is actually masked/unmasked
+        # check again target is actually masked/unmasked
+        $res = _target_is_masked("sleep.target");
+        unless ($res->[0] == 200) {
+            return $res;
+        }
+        $is_masked = $res->[2];
+        return [500, "Failed to mask sleep.target"]
+            if $which eq 'prevent' && !$is_masked;
+        return [304, "Failed to unmask sleep.target"]
+            if $which eq 'unprevent' && $is_masked;
 
         return [200, "OK", {'func.mechanism' => 'systemd'}];
     } # SYSTEMD
